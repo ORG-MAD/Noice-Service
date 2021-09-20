d56756ab327ce8c8ac2364d2201bb7119fe8209e
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2021-09-20 12:32:29 +0530

    exand mybook

---
+++ app/src/main/java/com/example/noice_service/MyBookingClickListner.java
@@ -0,0 +1,5 @@
+package com.example.noice_service;
+
+public interface MyBookingClickListner {
+    void onClickItem(MyBookings myBookings);
+}
---
+++ app/src/main/java/com/example/noice_service/MyBookingExpand.java
@@ -0,0 +1,34 @@
+package com.example.noice_service;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyBookingExpand extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_booking_expand);
+
+        TextView tvname = findViewById(R.id.mbooking_name);
+        TextView tvtime_slot = findViewById(R.id.time_slot);
+        TextView tvcar_no = findViewById(R.id.car_no);
+        TextView tvphone_no = findViewById(R.id.phone_no);
+
+        String id = getIntent().getStringExtra("booking_id");
+        String name = getIntent().getStringExtra("booking_name");
+        String time_slot = getIntent().getStringExtra("time_slot");
+        String car_no = getIntent().getStringExtra("car_no");
+        String phone_no = getIntent().getStringExtra("phone_no");
+
+
+
+        tvname.setText(name);
+        tvtime_slot.setText(time_slot);
+        tvcar_no.setText(car_no);
+        tvphone_no.setText(phone_no);
+
+    }
+}
